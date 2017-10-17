48434b0f69dbb8760417e00aa4cf0829c6b94996
eric98 <ergare.17@gmail.com>
2017-10-17 15:56:30 +0200

    Primer versió

---
+++ tests/ExampleTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Ergare17/Articles;
+
+class ExampleTest extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * Test that true does in fact equal true
+     */
+    public function testTrueIsTrue()
+    {
+        $this->assertTrue(true);
+    }
+}
