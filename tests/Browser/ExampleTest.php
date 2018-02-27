bb57b156d97fce5d12156b83e2f55c532d1e4561
eric98 <ergare.17@gmail.com>
2018-02-27 16:24:39 +0100

    Sync

---
+++ tests/Browser/ExampleTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Ergare17\Articles\Browser;
+
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+
+class ExampleTest extends DuskTestCase
+{
+    /**
+     * A basic browser test example.
+     *
+     * @return void
+     */
+    public function testBasicExample()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/')
+                    ->assertSee('Laravel');
+        });
+    }
+}
